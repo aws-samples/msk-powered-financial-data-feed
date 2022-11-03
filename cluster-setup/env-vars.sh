6d7dfaaebe61a27852f256bf5a4c70c0923cb40a
Rana Dutt <rdutt@amazon.com>
2022-11-03 00:41:22 +0000

    Update gitignore to ignore env-vars.sh which contains account-specific ARNs

--- cluster-setup/env-vars.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# 
-# Replace the below environment variable values
-#
-export ACM_PCA_ARN="ARN of your AWS Private Certificate Authority"
-export EC2_KEY_PAIR="Name of your EC2 keypair"
-
