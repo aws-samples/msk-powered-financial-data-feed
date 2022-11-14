c36525b50e3cdbcf901e5e334205f3be64b4a43d
Rana Dutt <rdutt@amazon.com>
2022-11-14 01:55:25 +0000

    Update README

--- nlb-setup/nlb_setup/env-vars.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# 
-# Replace the below environment variable values
-#
-export TLSBROKERS=b-2.mymskcluster.ljlmj5.c19.kafka.us-east-1.amazonaws.com:9094,b-1.mymskcluster.ljlmj5.c19.kafka.us-east-1.amazonaws.com:9094
-
