709bd978af288c6319d0916d650abe87bcb32ba7
soadiego <soadiego@amazon.com>
2022-11-29 13:54:18 -0800

    1

--- bin/update_cluster.py
+++ bin/update_cluster.py
@@ -27,19 +27,20 @@ while i <= number_of_nodes:
     print("###")
     output=json.loads(out.decode('utf-8'))
 
-    #print(json.dumps(output, indent=2, default=str))
+    print(json.dumps(output, indent=2, default=str))
 
     endpoints = output.get("endpoints")
     host = output.get("host")
+    host = str(host).replace("-internal","")
     protocol_map = output.get("listener_security_protocol_map")
     endpoints.append("CLIENT_SECURE_VPCE://"+str(host)+":"+str(init_port))
     protocol_map["CLIENT_SECURE_VPCE"] = "SSL"
 
     update_listener_part1 = "~/kafka/bin/kafka-configs.sh --bootstrap-server "+str(host)+":9094  --entity-type brokers --entity-name "+str(i)
-    update_listener_part2 = " --alter --command-config " + properties_file + " --add-config advertised.listeners="+str(endpoints)
+    update_listener_part2 = " --alter --command-config " + properties_file + " --add-config advertised.listeners="+str(endpoints).replace(" ","").replace("'","")
 
     update_map_part1 = "~/kafka/bin/kafka-configs.sh --bootstrap-server "+str(host)+":9094  --entity-type brokers --entity-name "+str(i)
-    update_map_part2 = " --alter --command-config " + properties_file + " --add-config listener.security.protocol.map=\""+str(protocol_map)+"\""
+    update_map_part2 = " --alter --command-config " + properties_file + " --add-config listener.security.protocol.map="+str(protocol_map).replace(" ","").replace("'","")
 
 
     print(update_listener_part1+update_listener_part2)
